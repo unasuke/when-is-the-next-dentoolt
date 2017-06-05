df8974d483fb9105092f7c433a7914ca32830e1a
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-06-05 18:48:32 +0900

    Remove hanami resources
    hanami is not lightweight waf

--- ruby/hanami/apps/web/config/routes.rb
+++
@@ -1,8 +0,0 @@
-# Configure your routes here
-# See: http://hanamirb.org/guides/routing/overview/
-#
-# Example:
-# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
-
-root to: 'home#index'
-get '/home(.:format)', to: 'home#index'
